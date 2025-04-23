2d192d9e6197be4162f6f8f37e5932654acea6ac
Tharindu Wijekoon <wijekoontharindu80@gmail.com>
2025-04-23 08:23:34 +0530

    Created some basic types

---
+++ neo4j-p2e2/src/main/java/org/example/dependency/Rule.java
@@ -0,0 +1,18 @@
+package org.example.dependency;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+
+public class Rule {
+    public Property head;
+    public ArrayList<Property> tail = new ArrayList<>(2);
+    public ArrayList<String> nodes = new ArrayList<>(1);
+    public LinkedHashMap<String, String> node2Alias = new LinkedHashMap<>(2);
+    public String condition;
+
+    @Override
+    public String toString() {
+        return head.property + " <- " + tail.stream().map(a -> a.property).collect(Collectors.joining());
+    }
+}
