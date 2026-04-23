31f31d6d6433ce8c709ed121a052f337ac969e0f
Tharindu Wijekoon <wijekoontharindu80@gmail.com>
2026-04-23 15:43:01 +0530

    Add InstantiatedModel and Instantiator classes, and create QueryRequest and QueryResponse DTOs

---
+++ neo4j-p2e2/src/main/java/org/example/dependency/Instantiator.java
@@ -0,0 +1,237 @@
+package org.example.dependency;
+
+import org.example.config.Neo4jProperties;
+import org.example.dependency.Cell;
+import org.example.dependency.Cell.HyperEdge;
+import org.example.dependency.Property;
+import org.example.dependency.Rule;
+import org.neo4j.driver.*;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.exceptions.Neo4jException;
+import org.neo4j.driver.summary.ResultSummary;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Instantiator {
+    final public HashMap<Property, ArrayList<Rule>> propertyInHead;
+    final public HashMap<Property, ArrayList<Rule>> propertyInTail;
+    final HashMap<String, String> nodeName2keyProp;
+    final ArrayList<Rule> EMPTY_LIST = new ArrayList<>(0);
+    final String IT_RELATION = "INSERTED_AT";
+
+    public final Driver driver;
+    public final SessionConfig sessionConfig;
+
+    public Instantiator(
+            HashMap<Property, ArrayList<Rule>> propertyInHead,
+            HashMap<Property, ArrayList<Rule>> propertyInTail,
+            HashMap<String, String> nodeName2keyProp,
+            Neo4jProperties neo4jProperties
+    ) throws Neo4jException {
+        this.propertyInHead = propertyInHead;
+        this.propertyInTail = propertyInTail;
+        this.nodeName2keyProp = nodeName2keyProp;
+
+        driver = GraphDatabase.driver(
+                neo4jProperties.uri,
+                AuthTokens.basic(
+                        neo4jProperties.username,
+                        neo4jProperties.password
+                )
+        );
+
+        sessionConfig = SessionConfig.builder()
+                .withDatabase(neo4jProperties.database)
+                .build();
+    }
+
+    public void close() {
+        driver.close();
+    }
+
+    public void completePropVal(Cell cell) throws Neo4jException {
+        var keyProp = nodeName2keyProp.get(cell.property.node);
+        var prop = cell.property.property;
+        var node = cell.property.node;
+        var key = cell.key;
+
+        try (Session session = driver.session(sessionConfig)) {
+            session.executeRead(tx -> {
+                String cypher = String.format(
+                        "MATCH (a:%s {%s:%s})-[:%s]->(b) RETURN a.%s AS aProp, b.%s AS bProp",
+                        node, keyProp, key, IT_RELATION, prop, prop
+                );
+
+                Result result = tx.run(cypher);
+                if (result.hasNext()) {
+                    Record record = result.next();
+                    cell.value = record.get("aProp").toString();
+                    cell.insertionTime = safeAsLong(record.get("bProp"));
+                }
+                if (result.hasNext()) {
+                    throw new Neo4jException("Non-unique key!");
+                }
+                return null;
+            });
+        }
+    }
+
+    public ArrayList<HyperEdge> instantiateAttachedCells(Cell start, long sourceInsertionTime) throws Neo4jException {
+        var result = new ArrayList<HyperEdge>();
+        iterateRules(start, sourceInsertionTime, result, propertyInHead);
+        iterateRules(start, sourceInsertionTime, result, propertyInTail);
+        return result;
+    }
+
+    public void iterateRules(Cell start, long sourceInsertionTime, ArrayList<HyperEdge> result, HashMap<Property, ArrayList<Rule>> connectedRules)
+            throws Neo4jException {
+        for (var rule : connectedRules.getOrDefault(start.property, EMPTY_LIST)) {
+            ArrayList<Record> rs = queryRule(rule, start, sourceInsertionTime);
+            result.addAll(resultSetToCellList(rule, start, rs, sourceInsertionTime));
+        }
+    }
+
+    public ArrayList<Record> queryRule(Rule rule, Cell identifier, long sourceInsertionTime) throws Neo4jException {
+        ArrayList<String> matchStrings = new ArrayList<>();
+        ArrayList<String> whereStrings = new ArrayList<>();
+        ArrayList<String> returnStrings = new ArrayList<>();
+
+        String id = identifier.key;
+        String identifierNode = identifier.property.node;
+        String nodeAlias = rule.node2Alias.get(identifierNode);
+        String nodeKey = nodeName2keyProp.get(identifierNode);
+        String idMatchCondition = "(" + nodeAlias + ":" + identifierNode + " {" + nodeKey + ":" + id + "})";
+
+        matchStrings.add(idMatchCondition);
+        matchStrings.add(rule.condition);
+
+        for (int i = 0; i < rule.nodes.size(); i++) {
+            String node = rule.nodes.get(i);
+            returnStrings.add(rule.node2Alias.get(node) + "." + nodeName2keyProp.get(node));
+        }
+
+        for (String node : rule.nodes) {
+            String alias = rule.node2Alias.get(node);
+            String alias_it = alias + "_it";
+            String matchCondition = "(" + alias + ")-[:" + IT_RELATION + "]->(" + alias_it + ")";
+            matchStrings.add(matchCondition);
+        }
+
+        String it = Long.toString(identifier.insertionTime);
+        String headAlias = rule.node2Alias.get(rule.head.node);
+        String headAlias_it = headAlias + "_it";
+        String headProp = rule.head.property;
+
+        whereStrings.add(headAlias_it + "." + headProp + ">=" + it);
+        returnStrings.add(headAlias + "." + headProp);
+        returnStrings.add(headAlias_it + "." + headProp);
+
+        for (Property tail : rule.tail) {
+            String tailAlias = rule.node2Alias.get(tail.node);
+            String tailAlias_it = tailAlias + "_it";
+            String tailProp = tail.property;
+
+            whereStrings.add(tailAlias_it + "." + tailProp + ">=" + it);
+            returnStrings.add(tailAlias + "." + tailProp);
+            returnStrings.add(tailAlias_it + "." + tailProp);
+        }
+
+        String finalQuery =
+                "MATCH " + String.join(", ", matchStrings) + " " +
+                        "WHERE " + String.join(" OR ", whereStrings) + " " +
+                        "RETURN " + String.join(", ", returnStrings) + ";";
+
+        try (Session session = driver.session(sessionConfig)) {
+            return session.executeRead(tx -> {
+                Result rs = tx.run(finalQuery);
+                ArrayList<Record> list = new ArrayList<>();
+                while (rs.hasNext()) {
+                    list.add(rs.next());
+                }
+                return list;
+            });
+        }
+    }
+
+    public ArrayList<HyperEdge> resultSetToCellList(Rule rule, Cell start, ArrayList<Record> records, long sourceInsertionTime) throws Neo4jException {
+        var result = new ArrayList<HyperEdge>();
+        for (Record record : records) {
+            HashMap<String, String> node2Key = new HashMap<>();
+            int columnIdx = 0;
+            for (int nodeIdx = 0; nodeIdx < rule.nodes.size(); nodeIdx++) {
+                node2Key.put(rule.nodes.get(nodeIdx), record.get(columnIdx++).toString());
+            }
+
+            if (rule.head.equals(start.property)) {
+                columnIdx += 2;
+                var list = new HyperEdge(rule.tail.size());
+                boolean anyNull = false;
+                for (int tailIdx = 0; tailIdx < rule.tail.size(); tailIdx++) {
+                    var currProp = rule.tail.get(tailIdx);
+                    String val = record.get(columnIdx++).toString();
+                    long it = safeAsLong(record.get(columnIdx++));
+                    if (val == null) {
+                        anyNull = true;
+                        break;
+                    }
+                    if (it >= sourceInsertionTime) {
+                        list.add(new Cell(currProp, node2Key.get(currProp.node), val));
+                    }
+                }
+                if (!anyNull && !list.isEmpty()) {
+                    result.add(list);
+                }
+            } else {
+                String val = record.get(columnIdx++).toString();
+                long it = safeAsLong(record.get(columnIdx++));
+                if (val != null && it >= sourceInsertionTime) {
+                    var list = new HyperEdge(1);
+                    list.add(new Cell(rule.head, node2Key.get(rule.head.node), val));
+                    result.add(list);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void deleteCells(HashSet<Cell> toDelete)
+            throws Neo4jException {
+
+        try (Session session = driver.session(sessionConfig)) {
+
+            session.executeWrite(tx -> {
+
+                for (var cell : toDelete) {
+
+                    String node = cell.property.node;
+                    String prop = cell.property.property;
+                    String keyProp =
+                            nodeName2keyProp.get(node);
+                    String key = cell.key;
+
+                    String query = String.format(
+                            "MATCH (a:%s {%s: %s}) REMOVE a.%s",
+                            node,
+                            keyProp,
+                            key,
+                            prop
+                    );
+
+                    tx.run(query);
+                }
+
+                return null;
+            });
+        }
+    }
+
+    private long safeAsLong(Value value) {
+        if (value.type().name().equals("STRING")) {
+            return Long.parseLong(value.asString());
+        }
+        return value.asLong();
+    }
+}
---
+++ neo4j-p2e2/src/main/java/org/example/dto/QueryResponse.java
@@ -0,0 +1,8 @@
+package org.example.dto;
+
+import lombok.Value;
+
+@Value
+public class QueryResponse {
+    String message;
+}
